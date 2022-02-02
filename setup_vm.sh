d339416f689de3ca031dfeaed70608fb9dec7bcf
Ari Porad <aporad@olin.edu>
2022-02-02 00:56:24 -0500

    chore: add command to cd into comprobo folder

--- setup_vm.sh
+++ setup_vm.sh
@@ -8,7 +8,7 @@
 # https://forum.parallels.com/threads/ros-rviz-opengl-problem-on-ubuntu-20-04.351631/page-2#post-899436
 export LIBGL_ALWAYS_SOFTWARE=1
 
-COMPROBO=$(cd $(dirname ${BASH_SOURCE}) > /dev/null; pwd)
+export COMPROBO=$(cd $(dirname ${BASH_SOURCE}) > /dev/null; pwd)
 CATKIN_WS=$(cd "$COMPROBO/../.." > /dev/null; pwd)
 
 # Make sure Gazebo can find our stuff in its weird place
@@ -20,3 +20,7 @@ source $CATKIN_WS/devel/setup.bash
 # Make sure ROS can find our stuff in its weird place
 # This must be after sourcing ROS's setup.bash
 export ROS_PACKAGE_PATH=$COMPROBO:$ROS_PACKAGE_PATH
+
+function comprobocd() {
+	cd $COMPROBO
+}
